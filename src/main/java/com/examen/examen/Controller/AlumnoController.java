d1a1c9b3fe1bfd24f8b32e38eacd9f9b5954971a
JVaquero2003 <95882403+JVaquero2003@users.noreply.github.com>
2023-03-13 09:59:27 +0100

    2º commit

--- src/main/java/com/examen/examen/Controller/AlumnoController.java
+++ src/main/java/com/examen/examen/Controller/AlumnoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.examen.examen.Model.Alumno;
 
@@ -26,7 +27,7 @@ public class AlumnoController {
     public AlumnoController() {
         Alumno alumno1 = new Alumno(1, "Jose", "SI");
         Alumno alumno2 = new Alumno(2, "Alex", "NO");
-        Alumno alumno3 = new Alumno(3, "Dani", "SI");
+        Alumno alumno3 = new Alumno(3, "Dani", "NO");
 
         alumnos.add(alumno1);
         alumnos.add(alumno2);
@@ -40,40 +41,47 @@ public class AlumnoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(alumno);
     }
 
-    @GetMapping(path = "/ALUMNOS/DELETE/")
-    public Alumno eliminarAlumno(@RequestParam int id) {
+    // 3 y 4
+    @GetMapping("/ALUMNOS/DELETE/")
+    public ResponseEntity<Alumno> eliminarAlumno(@RequestParam int id) {
+        if (id == 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
         for (int i = 0; i < alumnos.size(); i++) {
             if (alumnos.get(i).getId() == id) {
                 Alumno alumnoEliminado = alumnos.remove(i);
-                return alumnoEliminado;
+                return ResponseEntity.ok().body(alumnoEliminado);
             }
         }
         return null;
     }
 
-    @GetMapping(path = "/ALUMNOS/UPDATE/{id}/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Alumno> getAlumno(@PathVariable(value = "id") int alumnoId) throws RelationNotFoundException {
-        // Simulamos una consulta a la base de datos o a una fuente de datos externa
-        Alumno alumno = null;
+    @GetMapping("/ALUMNOS/UPDATE/{id}/")
+    public ResponseEntity<Alumno> updateAlumno(@PathVariable int id, @RequestParam String fct) {
+        Alumno alumnoActualizado = null;
         for (int i = 0; i < alumnos.size(); i++) {
-            if (alumnos.get(i).getId() == alumnoId) {
-                alumno = alumnos.get(i);
+            if (alumnos.get(i).getId() == id) {
+                alumnoActualizado = alumnos.get(i);
+                alumnoActualizado.setFct(fct);
+                alumnos.set(i, alumnoActualizado);
                 break;
             }
         }
-
-        // Comprobamos si el alumno existe
-        if (alumno == null) {
-            throw new RelationNotFoundException("Alumno no encontrado con el id: " + alumnoId);
+        if (alumnoActualizado != null) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(alumnoActualizado);
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
 
-        alumno.setFct("yes");
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(alumno);
+    @PostMapping(path = "/ALUMNOS/LIST", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<List<Alumno>> listarAlumnos() {
+        return ResponseEntity.status(HttpStatus.OK).body(alumnos);
     }
 
     @GetMapping(path = "/ALUMNOS/UPDATE/{id}/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Alumno> getAlumno2(@PathVariable(value = "id") int alumnoId)
-            throws RelationNotFoundException {
+    public ResponseEntity<Alumno> getAlumno(@PathVariable(value = "id") int alumnoId) throws RelationNotFoundException {
         // Simulamos una consulta a la base de datos o a una fuente de datos externa
         Alumno alumno = null;
         for (int i = 0; i < alumnos.size(); i++) {
@@ -94,6 +102,12 @@ public class AlumnoController {
         }
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(alumno);
+    }
+
+    @GetMapping("/ALUMNOS/DELETEALL")
+    public ResponseEntity<Alumno> deleteAll() {
+        alumnos.clear();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
